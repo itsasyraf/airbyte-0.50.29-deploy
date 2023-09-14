0de4b29b9082f8bd50fad60dfcd2ec639e55b18c
Asyraf Adianto <asyraf.adianto@gmail.com>
2023-09-14 10:49:35 +0000

    Let the code 'docker compose down' run before deploying new instance

--- airbyte-deploy.sh
+++ airbyte-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir airbyte-0.50.29 && git clone https://github.com/itsasyraf/airbyte-0.50.29-conf.git . && docker compose down && cd .. && rm -fr ./airbyte-0.50.29 
 git clone https://github.com/itsasyraf/airbyte-0.50.29.git
 git clone https://github.com/itsasyraf/airbyte-0.50.29-conf.git
 cd airbyte-0.50.29
@@ -12,8 +13,11 @@ cd airbyte-0.50.29
 ./run-ab-platform.sh -b
 cd ..
 rm -fr ./airbyte-0.50.29
-mkdir airbyte-0.50.29
-touch ./airbyte-0.50.29/docker-compose.yml
-cp ./airbyte-0.50.29-conf/docker-compose.yml ./airbyte-0.50.29/docker-compose.yml
-rm -fr ./airbyte-0.50.29-conf
+# mkdir airbyte-0.50.29
+# touch ./airbyte-0.50.29/docker-compose.yml
+# touch ./airbyte-0.50.29/.env
+# cp ./airbyte-0.50.29-conf/docker-compose.yml ./airbyte-0.50.29/docker-compose.yml
+# cp ./airbyte-0.50.29-conf/.env ./airbyte-0.50.29/.env
+# rm -fr ./airbyte-0.50.29-conf
+mv ./airbyte-0.50.29-conf ./airbyte-0.50.29
 
